7721ac05f1ff94e7ad22af1987068518a5318b55
kennytram <kennyvtram@gmail.com>
2024-08-17 16:51:53 -0700

    Update TarManager to contain one source directory at a time

--- TarManager.py
+++ TarManager.py
@@ -4,16 +4,16 @@ import tarfile
 
 
 class TarManager:
-    def __init__(self, source_dirs: set = set(), dest_dir: str = "./") -> None:
-        self.source_dirs = source_dirs
+
+    def __init__(self, source_dir: set = "./", dest_dir: str = "./") -> None:
+        self.source_dir = source_dir
         self.dest_dir = dest_dir
 
-    def get_file_paths(self, patterns: list) -> set:
-        for dir in self.source_dirs:
-            file_paths = set()
-            for pattern in patterns:
-                matched_files = glob.glob(os.path.join(dir, pattern), recursive=True)
-                file_paths.update(matched_files)
+    def get_file_patterns_paths(self, patterns: set) -> set:
+        file_paths = set()
+        for pattern in patterns:
+            matched_files = glob.glob(os.path.join(dir, pattern), recursive=True)
+            file_paths.update(matched_files)
         return file_paths
 
     def tar(self, tar_name: str, file_paths: set) -> None:
@@ -29,11 +29,12 @@ class TarManager:
                 filename = os.path.basename(file_path)
                 tar.write(file_path, arcname=filename)
 
-    def add(self, dir: str) -> None:
-        self.source_dirs.add(dir)
+    def archive(self, tar_name: str, file_patterns: list) -> None:
+        paths = self.get_file_patterns_paths(file_patterns)
+        self.tar(self, tar_name, paths)
 
-    def remove(self, dir: str) -> None:
-        self.source_dirs.discard(dir)
+    def set_source_dir(self, dir: str) -> None:
+        self.source_dir = dir
 
     def set_dest_dir(self, dest_dir: str) -> None:
         self.dest_dir = dest_dir

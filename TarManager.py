fa2ce93464ab8eb0cac65887bcd9613a3ff63231
kennytram <kennyvtram@gmail.com>
2024-08-17 13:30:03 -0700

    Replace lists with sets to prevent duplicates

--- TarManager.py
+++ TarManager.py
@@ -4,19 +4,19 @@ import tarfile
 
 
 class TarManager:
-    def __init__(self, source_dirs: list = [], dest_dir: str = "./") -> None:
+    def __init__(self, source_dirs: set = set(), dest_dir: str = "./") -> None:
         self.source_dirs = source_dirs
         self.dest_dir = dest_dir
 
-    def get_file_paths(self, patterns: list) -> list:
+    def get_file_paths(self, patterns: list) -> set:
         for dir in self.source_dirs:
-            file_paths = []
+            file_paths = set()
             for pattern in patterns:
                 matched_files = glob.glob(os.path.join(dir, pattern), recursive=True)
-                file_paths.extend(matched_files)
+                file_paths.update(matched_files)
         return file_paths
 
-    def tar(self, tar_name: str, file_paths: list) -> None:
+    def tar(self, tar_name: str, file_paths: set) -> None:
         if self.dest_dir:
             os.makedirs(self.dest_dir, exist_ok=True)
             tar_name = os.path.join(self.dest_dir, tar_name)
@@ -30,13 +30,10 @@ class TarManager:
                 tar.write(file_path, arcname=filename)
 
     def add(self, dir: str) -> None:
-        self.source_dirs.append(dir)
+        self.source_dirs.add(dir)
 
     def remove(self, dir: str) -> None:
-        try:
-            self.source_dirs.remove(dir)
-        except ValueError:
-            pass
+        self.source_dirs.discard(dir)
 
     def set_dest_dir(self, dest_dir: str) -> None:
         self.dest_dir = dest_dir
